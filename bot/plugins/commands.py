88c35d495bfe418f067cf005374ea5217c4a4b2d
robin2255 <83762091+robin2255@users.noreply.github.com>
2021-05-07 17:22:15 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'JOIN OUR CHANNEL', url="https://t.me/ott_movies_world"
                                 )
                         ]
                     ]
@@ -57,7 +57,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'JOIN OUR CHANNEL', url="https://t.me/ott_movies_world"
                                 )
                         ]
                     ]
@@ -76,7 +76,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'JOIN OUR CHANNEL', url="https://t.me/ott_movies_world"
                                 )
                         ]
                     ]
